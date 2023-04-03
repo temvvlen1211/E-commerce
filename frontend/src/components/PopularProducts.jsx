83780adb803c32bcf89bd21922320783714eb544
temvvlen1211 <temvvlen1211@gmail.com>
2023-04-03 20:05:49 +0800

    popular product

---
+++ frontend/src/components/PopularProducts.jsx
@@ -0,0 +1,83 @@
+export default function PopularProducts() {
+  return (
+    <>
+      <div class="container">
+        <h2>Popular Products</h2>
+        <div class="row">
+          <div class="col-md-3">
+            <div class="card">
+              <img class="card-img-top" src="product1.jpg" alt="Product 1" />
+              <div class="card-body">
+                <h3 class="card-title">Product 1</h3>
+                <p class="card-text">Description of Product 1</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="card">
+              <img class="card-img-top" src="product2.jpg" alt="Product 2" />
+              <div class="card-body">
+                <h3 class="card-title">Product 2</h3>
+                <p class="card-text">Description of Product 2</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="card">
+              <img class="card-img-top" src="product3.jpg" alt="Product 3" />
+              <div class="card-body">
+                <h3 class="card-title">Product 3</h3>
+                <p class="card-text">Description of Product 3</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="card">
+              <img class="card-img-top" src="product3.jpg" alt="Product 3" />
+              <div class="card-body">
+                <h3 class="card-title">Product 3</h3>
+                <p class="card-text">Description of Product 3</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="card">
+              <img class="card-img-top" src="product1.jpg" alt="Product 1" />
+              <div class="card-body">
+                <h3 class="card-title">Product 1</h3>
+                <p class="card-text">Description of Product 1</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="card">
+              <img class="card-img-top" src="product2.jpg" alt="Product 2" />
+              <div class="card-body">
+                <h3 class="card-title">Product 2</h3>
+                <p class="card-text">Description of Product 2</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="card">
+              <img class="card-img-top" src="product3.jpg" alt="Product 3" />
+              <div class="card-body">
+                <h3 class="card-title">Product 3</h3>
+                <p class="card-text">Description of Product 3</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="card">
+              <img class="card-img-top" src="product3.jpg" alt="Product 3" />
+              <div class="card-body">
+                <h3 class="card-title">Product 3</h3>
+                <p class="card-text">Description of Product 3</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import CameraSection from "../components/CameraSection";
 import Navbar from "../components/Navbar";
+import PopularProducts from "../components/PopularProducts";
 import ProductSection from "../components/ProductSection";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar />
       <CameraSection />
       <ProductSection />
+      <PopularProducts />
     </>
   );
 }
